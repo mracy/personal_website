b82d70995ba46536905ad5972b3a1c8fbaad3300
Aashish CH <e.vibe3@gmail.com>
2024-02-08 10:58:14 +0545

    build url corrected

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build:{
-    base: "personal_website",
-    outDir: "./docs"
+    base: "/personal_website/",
+    outDir: "./docs",
   }
 })
