5ca97c5610cb2b201ede6a7e0cfc4f14fcdc74de
Aashish CH <e.vibe3@gmail.com>
2024-02-08 11:31:13 +0545

    Hire me button corrected

--- src/components/Expertise.jsx
+++ src/components/Expertise.jsx
@@ -22,18 +22,20 @@ const skills = [
 
 const Expertise = () => {
   return (
-    <div id="expertiseSection" className="mt-2" style={{ paddingTop: '100px' }}>
+    <div id="expertiseSection" className="mt-2">
       <h1 className="mb-16 text-5xl font-bold underline text-center">My Expertise</h1>
       {/* box section */}
       <div
         style={{
           backgroundImage: `url(${bannerBackground})`,
-          backgroundSize: 'cover',
+          backgroundSize: 'cover', // Set background size to 50% width and auto for the height
+
         }}
         className="box-container items-center flex flex-col md:flex-row py-16"
       >
         {/* text container */}
         <div className="flex text-white justify-center md:w-1/2">
+          {/* Add content if needed */}
           <div className="w-2/3 text-center space-y-4">
             <h1 className="text-4xl font-bold">I love these technologies</h1>
             <p>
@@ -41,7 +43,13 @@ const Expertise = () => {
               officia aperiam asperiores quasi, nostrum dolorum nemo facilis, doloremque ipsam
               voluptatum eveniet modi vero reiciendis quia aspernatur? Vero.
             </p>
-            {/* You can add more content if needed */}
+            <button
+              className="text-2xl px-4 py-2 bg-orange-500 rounded-full shadow-lg transition duration-300 hover:bg-orange-600"
+              onClick={() => console.log("Button clicked")}
+            >
+              Hire Me
+            </button>
+
           </div>
         </div>
 
