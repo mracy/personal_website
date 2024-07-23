270603255570adc4c0fd8449d593f28d433a31e9
mracy <e.vibe3@gmail.com>
2024-07-23 18:43:28 +0545

    Cv button fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -43,26 +43,24 @@ const Header = () => {
     <div className={`fixed top-0 left-0 right-0 bg-gray-100 p-4 z-50 ${isSmallScreen ? 'opacity-80' : ''}`} style={{ fontFamily: 'Roboto, sans-serif' }}>
       <div className="flex flex-row justify-between items-center">
         {/* Brand Name and Menu Links Container */}
-        <div className="flex items-center"> {/* Add a flex container */}
+        <div className="flex items-center">
           <div className="text-center">
             <h1 className="text-2xl font-light">{brandName}</h1>
           </div>
 
-          {!isSmallScreen && ( /* Render menuLinks section only for large screens */
+          {!isSmallScreen && (
             <div className={`md:flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-6 ${isMenuOpen ? 'block' : 'hidden'}`} style={{ paddingLeft: '300px' }}>
-
               {menuLinks.map((link) => (
-                <div key={link.id}> {/* Wrap each link in a div */}
+                <div key={link.id}>
                   <ScrollLink
                     to={link.link}
                     smooth={true}
                     duration={200}
                     className="hover:text-orange-600 cursor-pointer font-semibold"
-                    onClick={() => setIsMenuOpen(false)} // Close the menu on link click
+                    onClick={() => setIsMenuOpen(false)}
                   >
                     {link.title}
                   </ScrollLink>
-                  {/* Add a line break after each link */}
                 </div>
               ))}
             </div>
@@ -70,19 +68,17 @@ const Header = () => {
         </div>
 
         {/* Download CV Button */}
-        <div className="flex items-center"> {/* Add a flex container */}
-          <div className="mt-4 md:mt-0 mx-auto"> {/* Center the button */}
-            <button
-              onClick={handleDownloadCV}
-              className="px-4 py-2 bg-orange-500 shadow rounded-full text-xl text-white hover:bg-orange-600 hover:text-white transition duration-300"
-            >
-              Download CV
-            </button>
-          </div>
+        <div className="flex items-center">
+          <button
+            onClick={handleDownloadCV}
+            className="px-3 py-1 sm:px-4 sm:py-2 md:px-6 md:py-3 bg-orange-500 shadow rounded-full text-sm sm:text-base md:text-lg text-white hover:bg-orange-600 hover:text-white transition duration-300 max-w-xs"
+          >
+            Download CV
+          </button>
         </div>
 
         {/* Hamburger Icon */}
-        {isSmallScreen && ( /* Render FaBars only on small screens */
+        {isSmallScreen && (
           <div>
             <FaBars onClick={handleMenuClick} className="cursor-pointer" size={isMenuOpen ? 28 : 20} />
           </div>
@@ -90,7 +86,6 @@ const Header = () => {
       </div>
 
       {isMenuOpen && isSmallScreen && (
-        /* Render menuLinks section only on small screens when menu is open */
         <div className="bg-gray mt-4 p-4">
           {menuLinks.map((link, index) => (
             <div key={link.id} className="mb-2">
@@ -101,27 +96,23 @@ const Header = () => {
                 className="hover:text-orange-600 cursor-pointer font-semibold"
                 onClick={(e) => {
                   if (isSmallScreen) {
-                    e.preventDefault(); // Prevent default scroll behavior
+                    e.preventDefault();
                     return;
                   }
-                  setIsMenuOpen(false); // Close the menu on link click for large screens
+                  setIsMenuOpen(false);
                 }}
               >
                 {link.title}
               </ScrollLink>
               {index !== menuLinks.length - 1 && (
-                /* Add a line after each link except the last one */
                 <div className="border-b border-gray-300"></div>
               )}
             </div>
           ))}
         </div>
       )}
-
     </div>
   );
-
-
 };
 
 export default Header;
