5e9fa29b0c05c7c9825e3fd8fb5232710d9d4009
mracy <e.vibe3@gmail.com>
2024-07-23 20:58:28 +0545

    fixed button for small size devices

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -71,7 +71,7 @@ const Header = () => {
         <div className="flex items-center">
           <button
             onClick={handleDownloadCV}
-            className="px-3 py-1 sm:px-4 sm:py-2 md:px-6 md:py-3 bg-orange-500 shadow rounded-full text-sm sm:text-base md:text-lg text-white hover:bg-orange-600 hover:text-white transition duration-300 max-w-xs"
+            className={`px-4 py-2 sm:px-5 sm:py-3 md:px-6 md:py-3 bg-orange-500 shadow rounded-full text-sm sm:text-base md:text-lg text-white hover:bg-orange-600 hover:text-white transition duration-300 ${isSmallScreen ? 'text-base px-5 py-3' : 'max-w-xs'}`}
           >
             Download CV
           </button>
